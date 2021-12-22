b95c763b5ade5bf2006659e0b75411a8380ea46c
Hasene Ceren Yıkılmaz <78276871+hcyikilmaz@users.noreply.github.com>
2021-12-23 01:07:33 +0300

    Update find_unused_dbs.py
    oa is not none added

--- find_unused_dbs.py
+++ find_unused_dbs.py
@@ -87,6 +87,7 @@ for db in dbs:
                         open_app = doc.get('event', None)
                         if open_app is not None:
                             oa = open_app.get('n:oa')
+                        if oa is not None:
                             oa_total = oa.get('TOTAL')
                             print("Open App: " + str(oa_total))
 
