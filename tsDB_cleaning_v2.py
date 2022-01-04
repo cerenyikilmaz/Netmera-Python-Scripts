3d7bae75e7b0d978dd20bbb1e5f4e4e39e446c04
cerenyikilmaz <cerenyikilmaz@hotmail.com>
2022-01-04 15:33:41 +0300

    update

--- tsDB_cleaning_v2.py
+++ tsDB_cleaning_v2.py
@@ -12,48 +12,36 @@ automationDbNames = []
 mongoMessageClient = MongoClient(os.environ["python_env_mongo2"])
 messageDbs = mongoMessageClient.list_database_names()
 
-# mongo automationdan tsDB'si olan dbleri al.
+# Get dbs which have tsDB collection
 
 for db in automationDbs:
-    if db.startswith("tsDB"): # tsDB_fizy => ['tsDB', 'fizy']
+    if db.startswith("tsDB"): 
         dbName = db.split("_")[1]
         automationDbNames.append(dbName)
 
-#print(automationDbNames)
-
-# mongo'da olan dblerini bul
+# Find dbs which exists in Mongo
 
 for automationDbName in automationDbNames:
     for messageDbName in messageDbs:
 
-        # iki yerde de varsa;
+        # If it's exists both
 
         if automationDbName == messageDbName:
 
-            #print(automationDbName)
-            #print(messageDbName)
-
             mydb = mongoMessageClient[messageDbName]
-            #print(mydb)
             mycol = mydb["message"]
-            #print(mycol)
 
             today = datetime.datetime.utcnow()
             nDayAgo = datetime.timedelta(days = 100)
             deltaDate = today - nDayAgo
 
-            #message mongosuna bağlan
+            # Connect to message mongo 
 
             for doc in mycol.find({"endDate" : {"$gt" : deltaDate}, "msgMethod" : "AUTOMATED", "sendStatus": "DEACTIVE"}, {"triggerId" : 1, "id" : 1}):
-                #print("dbName: " + messageDbName + " Id: " + str(doc.get('_id')) + " TriggerId: " + str(doc.get('triggerId')))
-                # automation mongoda count
-                
+                                
                 mydb2 = mongoAutomationClient["tsDB_" + messageDbName]
-                #print(mydb2)
                 mycol2 = mydb2["tsn_"+str(doc.get('triggerId'))]
-                #print(mycol2)
                 count = mycol2.count()
-                #print(count)
 
                 if count > 0:
                     print(mycol2)
