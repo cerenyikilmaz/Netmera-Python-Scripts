84a8404023fb3ffeed34a44cea2871c0192998af
cerenyikilmaz <cerenyikilmaz@hotmail.com>
2022-01-14 07:38:25 +0300

    adding drop

--- tsDB_cleaning_v2.py
+++ tsDB_cleaning_v2.py
@@ -1,9 +1,22 @@
 import collections
-from os import sched_get_priority_max
+from os import lseek, sched_get_priority_max
 from re import M, S
 from pymongo.mongo_client import MongoClient
 import os
 import datetime
+import logging
+
+# ********* logging *********
+
+logging.basicConfig(filename="tsDB_cleaning.log",
+                    format='%(asctime)s [%(levelname)s] %(message)s',
+                    filemode='w')
+
+log = logging.getLogger()
+
+log.setLevel(logging.DEBUG)
+
+# ********* mongo clients *********
 
 mongoAutomationClient = MongoClient(os.environ["python_env_mongo"])
 automationDbs = mongoAutomationClient.list_database_names()
@@ -12,30 +25,25 @@ automationDbNames = []
 mongoMessageClient = MongoClient(os.environ["python_env_mongo2"])
 messageDbs = mongoMessageClient.list_database_names()
 
-# Get dbs which have tsDB collection
+# Get databases in MongoAutomation, which have tsDB collection, split and get appkey
 
 for db in automationDbs:
     if db.startswith("tsDB"): 
         dbName = db.split("_")[1]
         automationDbNames.append(dbName)
 
-# Find dbs which exists in Mongo
+# Compare clients, which have tsDB database in MongoAutomation and also have databse in Mongo
 
 for automationDbName in automationDbNames:
     for messageDbName in messageDbs:
-
-        # If it's exists both
-
         if automationDbName == messageDbName:
 
             mydb = mongoMessageClient[messageDbName]
             mycol = mydb["message"]
 
             today = datetime.datetime.utcnow()
-            nDayAgo = datetime.timedelta(days = 10)
+            nDayAgo = datetime.timedelta(days = 150)
             deltaDate = today - nDayAgo
-
-            # Connect to message mongo 
 
             for doc in mycol.find({"endDate" : {"$lt" : deltaDate}, "msgMethod" : "AUTOMATED", "sendStatus": "DEACTIVE"}, {"triggerId" : 1, "id" : 1}):
                                 
@@ -43,9 +51,21 @@ for automationDbName in automationDbNames:
                 mycol2 = mydb2["tsn_"+str(doc.get('triggerId'))]
                 count = mycol2.count()
 
+                # Split tsn_name for printing.
+
+                tsn_name = str(mycol2).split(" ")[5]
+                tsn_name = tsn_name.split("'")[1]
+
                 if count > 0:
-                    print(mycol2)
-                    print("dbName: " + messageDbName + " Id: " + str(doc.get('_id')) + " TriggerId: " + str(doc.get('triggerId')) + " Count: " + str(count) )
-            
 
+                    log.info("dbName: " + messageDbName + " Id: " + str(doc.get('_id')) + " TriggerId: " + str(doc.get('triggerId')) + " Count: " + str(count) + " tsnName: " + tsn_name)
+                    log.info(tsn_name + " collection in tsDB_" + messageDbName + " is dropping..")
 
+                    mycol2.drop()
+
+                    log.info(tsn_name + " collection in tsDB_" + messageDbName + " is dropped!")
+
+                else:
+
+                    log.info("dbName: " + messageDbName + " Id: " + str(doc.get('_id')) + " TriggerId: " + str(doc.get('triggerId')) + " Count: " + str(count) + " tsnName: " + tsn_name)
+                    log.info("Count equal zero, skipping.." + " tsnName: " + tsn_name)
